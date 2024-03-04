25c4393f4b6a2ea8d0977020ead4136b8b952cb9
priya-shruti <priya.shruti11@gmail.com>
2024-03-04 22:55:58 +0530

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $("#stylebutton").click(function () {
-        $("#box").css({ "background": "pink","color":"red" })
+    $("#appendBtn").click(function () {
+        $("#box").append("<h2>Yahoo</h2>")
     });
 });
